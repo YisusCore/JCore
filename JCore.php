2bb297f49614914786deea0bd8ba6ab578acca19
YisusCore <43710620+YisusCore@users.noreply.github.com>
2019-07-07 16:54:28 -0500

    Branch Variables

--- JCore.php
+++ JCore.php
@@ -24,3 +24,18 @@ isset($JC) or
 defined('STDIN') and 
 	chdir(APPPATH);
 
+/**
+ * Excute Time Start
+ *
+ * Indicate the exactly time what was loaded this file
+ * Used for BranchTimer
+ */
+$JC['ETS'] = microtime(TRUE);
+
+/**
+ * Excute Memory Start
+ *
+ * Indicate the exactly memory what was loaded this file
+  */
+$JC['EMS'] = memory_get_usage(TRUE);
+
