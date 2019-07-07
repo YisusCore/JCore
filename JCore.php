28efee95e3b8542e138bb21da1fc0d3f80c71fe3
YisusCore <43710620+YisusCore@users.noreply.github.com>
2019-07-07 16:52:46 -0500

    Update JCore.php

--- JCore.php
+++ JCore.php
@@ -5,6 +5,19 @@
  *
  * @filesource
  */
+
+/**
+ * VARIABLE JCore
+ *
+ * Variable global que permite almacenar valores y datos de manera global 
+ * sin necesidad de almacenarlo en una sesión u otra variable posiblemente 
+ * no existente
+ *
+ * @global
+ */
+isset($JC) or
+	$JC = [];
+
 /** 
  * Corrigiendo directorio base cuando se ejecuta como comando
  */
