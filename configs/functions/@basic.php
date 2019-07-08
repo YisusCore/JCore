45cff36fc1931438bc79585c9320a760fe1f7078
YisusCore <43710620+YisusCore@users.noreply.github.com>
2019-07-08 12:43:36 -0500

    Update @basic.php

--- configs/functions/@basic.php
+++ configs/functions/@basic.php
@@ -6,5 +6,13 @@
  * @filesource
  */
 
+/**
+ * DIRECTORY_SEPARATOR
+ *
+ * Separador de Directorios para el sistema operativo de ejecución
+ *
+ * @global
+ */
 defined('DS') or 
 	define('DS', DIRECTORY_SEPARATOR);
+
