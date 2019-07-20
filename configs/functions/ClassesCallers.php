09c8e4fddf82ba219984bb314cd6569527554a8c
YisusCore <43710620+YisusCore@users.noreply.github.com>
2019-07-20 13:17:55 -0500

    Update ClassesCallers.php

--- configs/functions/ClassesCallers.php
+++ configs/functions/ClassesCallers.php
@@ -5,3 +5,44 @@
  * @filesource
  */
 
+if ( ! function_exists('APP'))
+{
+	/**
+	 * APP()
+	 * Función que retorna la instancia de la clase APP
+	 *
+	 * @return App
+	 */
+	function APP ()
+	{
+		return APP::instance();
+	}
+}
+
+if ( ! function_exists('RQS'))
+{
+	/**
+	 * RQS()
+	 * Función que retorna la instancia de la clase Request
+	 *
+	 * @return Request
+	 */
+	function RQS ()
+	{
+		return Request::instance();
+	}
+}
+
+if ( ! function_exists('RSP'))
+{
+	/**
+	 * RSP()
+	 * Función que retorna la instancia de la clase Response
+	 *
+	 * @return Response
+	 */
+	function RSP ()
+	{
+		return Response::instance();
+	}
+}
