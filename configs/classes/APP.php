6a510225a2dc557c65701aefe25864b0b098dfd6
YisusCore <43710620+YisusCore@users.noreply.github.com>
2019-07-21 11:48:08 -0500

    Update APP.php

--- configs/classes/APP.php
+++ configs/classes/APP.php
@@ -11,15 +11,40 @@ class APP extends JArray
 	//-------------------------------------------
 	// Statics
 	//-------------------------------------------
+	private static $_initialized = false;
 	static function &instance()
 	{
 		static $instance;
 
-		isset($instance) or $instance = new self();
+		isset($instance) or 
+		$instance = new self();
+
+		! $instance::$_initialized and 
+		$instance->init() and
+		$instance::$_initialized = TRUE;
 
 		return $instance;
 	}
 
+	protected $_config;
+	protected function init ()
+	{
+		$this->_config =& config('array');
+		
+		foreach (['charset', 'timezone', 'lang'] as $index)
+		{
+			$this->_callbacks['set_' . $index] = function ($newval) use ($index)
+			{
+				$this->_config[$index] = $newval;
+			};
+
+			$this->_callbacks['get_' . $index] = function (&$return) use ($index)
+			{
+				$return = $this->_config[$index];
+			};
+		}
+		
+	}
 	
 	/**
      * __invoke ()
