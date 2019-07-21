097ff8deef74b2047004989c0dd1d17486d2cd8c
YisusCore <43710620+YisusCore@users.noreply.github.com>
2019-07-21 13:28:48 -0500

    Update APP.php

--- configs/classes/APP.php
+++ configs/classes/APP.php
@@ -31,18 +31,60 @@ class APP extends JArray
 	{
 		$this->_config =& config('array');
 		
-		foreach (['charset', 'timezone', 'lang'] as $index)
+		## Callback para el charset
+		$this->_callbacks['set_charset'] = function ($charset)
 		{
-			$this->_callbacks['set_' . $index] = function ($newval) use ($index)
-			{
-				$this->_config[$index] = $newval;
-			};
+			$this->_config['charset'] = $charset;
+			
+			## Convirtiendolo a mayúsculas
+			$charset = mb_strtoupper($charset);
 
-			$this->_callbacks['get_' . $index] = function (&$return) use ($index)
-			{
-				$return = $this->_config[$index];
-			};
-		}
+			## Estableciendo los charsets a todo lo que corresponde
+			ini_set('default_charset', $charset);
+			ini_set('php.internal_encoding', $charset);
+
+			@ini_set('mbstring.internal_encoding', $charset);
+			mb_substitute_character('none');
+
+			@ini_set('iconv.internal_encoding', $charset);
+		};
+
+		$this->_callbacks['get_charset'] = function (&$return)
+		{
+			$return = $this->_config['charset'];
+		};
+		
+		$this['charset'] = $this->_config['charset'];
+		
+		## Callback para el timezone
+		$this->_callbacks['set_timezone'] = function ($timezone)
+		{
+			$this->_config['timezone'] = $timezone;
+			
+			## Estableciendo el timezone a todo lo que corresponde
+			date_default_timezone_set($timezone);
+		};
+
+		$this->_callbacks['get_timezone'] = function (&$return)
+		{
+			$return = $this->_config['timezone'];
+		};
+		
+		$this['timezone'] = $this->_config['timezone'];
+		
+		## Callback para el lang
+		$this->_callbacks['set_lang'] = function ($lang)
+		{
+			$this->_config['lang'] = $lang;
+		};
+
+		$this->_callbacks['get_lang'] = function (&$return)
+		{
+			$return = $this->_config['lang'];
+		};
+		
+		$this['lang'] = $this->_config['lang'];
+		
 		
 	}
 	
